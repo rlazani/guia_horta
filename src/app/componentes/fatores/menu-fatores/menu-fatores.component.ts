1dcd0b7a3965689e4b680c2578c679979eab5f85
rlazani <rlazani@hotmail.com>
2024-04-09 13:37:43 -0300

    adicionando link no menu de fatores

--- src/app/componentes/fatores/menu-fatores/menu-fatores.component.ts
+++ src/app/componentes/fatores/menu-fatores/menu-fatores.component.ts
@@ -1,47 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Fator } from '../fator';
+import { Component, Input, OnInit } from "@angular/core";
+import { Fator } from "../fator";
 
 @Component({
-  selector: 'app-menu-fatores',
-  templateUrl: './menu-fatores.component.html',
-  styleUrls: ['./menu-fatores.component.css'],
+  selector: "app-menu-fatores",
+  templateUrl: "./menu-fatores.component.html",
+  styleUrls: ["./menu-fatores.component.css"],
 })
 export class MenuFatoresComponent implements OnInit {
+  abrirFicha(item: Fator) {
+    console.log(item);
+  }
   listaFatores: Fator[] = [
     {
       id: 1,
-      titulo: 'Espaçamento',
-      par1: '',
-      par2: '',
-      par3: '',
+      titulo: "Espaçamento",
+      par1: "",
+      par2: "",
+      par3: "",
     },
     {
       id: 2,
-      titulo: 'Solo',
-      par1: '',
-      par2: '',
-      par3: '',
+      titulo: "Solo",
+      par1: "",
+      par2: "",
+      par3: "",
     },
     {
       id: 3,
-      titulo: 'Iluminação',
-      par1: '',
-      par2: '',
-      par3: '',
+      titulo: "Iluminação",
+      par1: "",
+      par2: "",
+      par3: "",
     },
     {
       id: 4,
-      titulo: 'Irrigação',
-      par1: '',
-      par2: '',
-      par3: '',
+      titulo: "Irrigação",
+      par1: "",
+      par2: "",
+      par3: "",
     },
     {
       id: 5,
-      titulo: 'Pragas',
-      par1: '',
-      par2: '',
-      par3: '',
+      titulo: "Pragas",
+      par1: "",
+      par2: "",
+      par3: "",
     },
   ];
   constructor() {}
