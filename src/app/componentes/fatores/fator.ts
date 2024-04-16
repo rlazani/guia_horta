6a28369df2990b5dbbcd4453773fc8c643192f92
rlazani <rlazani@hotmail.com>
2024-04-16 13:21:30 -0300

    adicionando textos para os fatores ambientais

--- src/app/componentes/fatores/fator.ts
+++ src/app/componentes/fatores/fator.ts
@@ -4,5 +4,12 @@ export interface Fator {
   par1: string;
   par2: string;
   par3: string;
+  par4: string;
+  par5: string;
+  par6: string;
+  par7: string;
+  par8: string;
+  par9: string;
+  par10: string;
   img: string;
 }
