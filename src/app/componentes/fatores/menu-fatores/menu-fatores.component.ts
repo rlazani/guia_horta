a82a4eac46ec2140a9a8572fa369253c528e301d
rlazani <rlazani@hotmail.com>
2024-04-22 10:48:59 -0300

    adicionando funcionalidades

--- src/app/componentes/fatores/menu-fatores/menu-fatores.component.ts
+++ src/app/componentes/fatores/menu-fatores/menu-fatores.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Fator } from "../fator";
 import { FatorService } from "../fator.service";
 
